db62ad3e502fad1a63940906f12b1f543a6bc6ee
kfwolf77 <67338818+kfwolf77@users.noreply.github.com>
2020-08-17 00:20:21 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    tokens: ["NzA2NTkwMDY0OTg5MzA2OTkx.XzLkpg.6Ip1R41CA5y1Ftg_HhnvxmjUVAY","NzA2NTc5NTU2NDMzMzk1Nzgz.Xq8Tpw.Ts_Ojv8x4AilJaUATWWYOJjsKX8","NzA2NTc5OTkxNjgwNTE2MTc3.Xq8VHg.SH-BPUP_BAly2AiPL7lMFG4x9PQ","NzA2NTg0MTk5NzI5MTE5Mjg2.Xq8X9w.shY7x-Nz6N9eiZIsSRUqZMTsobw","NzA2NTkwMDY0OTg5MzA2OTkx.Xq8daQ.eaN4MJfR6hoFMoPnKBxhkb4n2gE","NzA2NTkwNjk3MDQ1ODg1MDEw.Xq8d9w.dLWt0avmDKsX7jNoNCdmShDVv-s","NzA2NTkxMjQ0MjA5NjE4OTg0.Xq8efQ.sO0b0JsV4di0oCpfyUs5Fc_nH5w","NzA2NTkzMDYxNDc5OTA3NDUx.Xq8gQg.bfb-ipGrRnk9NVzaJ1JxxRvvQEk","NzA2NTkzNTMwMjYwMjI2MDYx.Xq8gzA.pO9Da8KtPKXhAKuO_xpIpvu4DEs","NzA2NTk0MjM3ODg1NDQ4Mjcz.Xq8hbg.yKB2u9btpSCn4FO5OYtWqKChWFI","NzA2NTk0OTE4MDUwODI0MjE0.Xq8h9A.67Zvc0Ai4OW1DAbGznUq29toxL0","NzA2NTk1NTI1ODEzODYyNDEw.Xq8igQ.perfk1ycCuS03htgcac-K4g8T3Y","","token","token","token"], ///توكنات حسابات الترحيب
+    voice_channel: "732207509732851712", ///ايدي الروم اللي بدك يثبت فيه
+    text_channel: "737824409145966674", ///ايدي روم اللي بدك يرحب فيه
+    your_server: "462658139699937311", /// ايدي السيرفر تبعك
+}
---
+++ server.js
@@ -0,0 +1,33 @@
+const {
+  tokens,
+  voice_channel,
+  text_channel,
+  your_server
+} = require("./config.js");
+const time = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000];
+const Discord = require("discord.js");
+tokens.forEach(token => {
+  const client = new Discord.Client();
+  client.login(token);
+
+  client.on("message", async message => {
+    if (message.guild.id != your_server) return;
+    if (
+      message.author.id == "282859044593598464" && // تعديل مهم ايدي بوت الترحيب
+      message.content.includes("**𝐖𝐄𝐋𝐂𝐎𝐌𝐄 To 𝕜𝕗 Team**") /// (الاستقبال) تعديل مهم رسالة الترحيب
+    ) {
+      setTimeout(() => {
+        message.channel.send("**𝐖𝐄𝐋𝐂𝐎𝐌𝐄 To 𝕜𝕗 Team**"); ///  (الارسال)تعديل مهم رسالة الترحيب
+      }, time[Math.floor(Math.random() * time.length)]);
+    }
+  });
+
+  client.on("ready", () => {
+    console.log(client.user.tag + " is ready!");
+    client.guilds
+      .get(your_server)
+      .channels.get(voice_channel)
+      .join();
+  });
+
+});
